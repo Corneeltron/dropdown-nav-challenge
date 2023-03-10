aa4cad9990897c2a6eb62c50de757a2f4ac24e4f
corneliusphanthanh <cornelius.phanthanh@pnmac.com>
2023-03-10 09:30:58 +0900

    just gitout framer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,31 @@
 import { default as Link } from "next/link";
 import styles from "./Navigation.module.css";
-import {PopLink} from "./PopLink";
+import { PopLink } from "./PopLink";
 
+const linksLeft = [
+  { href: "/", title: "Home" },
+  { href: "Features", title: "Features" },
+  { href: "Company", title: "Company" },
+  { href: "About", title: "About" },
+];
 
+const linksRight = [
+  { href: "Login", title: "Login" },
+  { href: "Register", title: "Register" },
+]
 
 const Navigation = () => {
   return (
     <div className={styles.container}>
       <div className={styles.navLeft}>
-        <PopLink href="/" title="Pop" />
-        <PopLink href="/Features" title="Features" />
-        <PopLink href="/Company" title="Company" />
-        <PopLink href="/About" title="About" />
+        {linksLeft.map((link, i) => {
+          return <Link key={i} href={link.href}>{link.title}</Link>;
+        })}
       </div>
       <div className={styles.navRight}>
-        <Link href="/Login">Login</Link>
-        <Link href="/Register">Register</Link>
+        {linksRight.map((link) => {
+          return <Link href={link.href}>{link.title}</Link>;
+        })}
       </div>
     </div>
   );
